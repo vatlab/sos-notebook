a2d84bc1da808768c9849a8c054b1d51651c7300
Bo Peng <ben.bog@gmail.com>
2018-02-09 18:17:46 -0600

    Release sos notebook 0.9.12.0 for recent changes on sos and bug fixes to sos notebook

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(name = "sos-notebook",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires=[
-          'sos>=0.9.11.4',
+          'sos>=0.9.12.1',
           'nbformat',
           'nbconvert>=5.1.1',
           'ipython',
