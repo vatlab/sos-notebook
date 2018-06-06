fd2d939e8add0402a4836707306a9c3325bcb7cf
Bo Peng <ben.bog@gmail.com>
2018-06-06 09:43:10 -0500

    Release sos ntoebook 0.9.14.7 due to changes on sos 0.9.14.5

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(name="sos-notebook",
       packages=find_packages('src'),
       package_dir={'': 'src'},
       install_requires=[
-          'sos>=0.9.14.4',
+          'sos>=0.9.14.5',
           'nbformat',
           'nbconvert>=5.1.1',
           'ipython',
