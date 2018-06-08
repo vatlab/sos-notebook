427a91f241d15627dc32bbad7f43e2147da183b5
Bo Peng <ben.bog@gmail.com>
2018-06-08 00:20:08 -0500

    Release sos notebook 0.9.14.8 to be compatible with sos

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(name="sos-notebook",
       packages=find_packages('src'),
       package_dir={'': 'src'},
       install_requires=[
-          'sos>=0.9.14.5',
+          'sos>=0.9.14.6',
           'nbformat',
           'nbconvert>=5.1.1',
           'ipython',
