dc200e4f97b356290f78b41fe861e3f3bb38e5a9
Bo Peng <ben.bog@gmail.com>
2018-11-06 18:02:48 -0600

    Release sos notebook 0.17.1

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(name="sos-notebook",
       packages=find_packages('src'),
       package_dir={'': 'src'},
       install_requires=[
-          'sos>=0.17.0',
+          'sos>=0.17.4',
           'nbformat',
           'nbconvert>=5.1.1',
           'ipython',
