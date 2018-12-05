f9575cafbe84920bf11b358bab61b951b80ec21a
Bo Peng <ben.bog@gmail.com>
2018-12-05 11:23:32 -0600

    Release sos notebook 0.17.3

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(name="sos-notebook",
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
-          'License :: OSI Approved :: BSD',
+          'License :: OSI Approved :: BSD License',
           'Natural Language :: English',
           'Operating System :: POSIX :: Linux',
           'Operating System :: MacOS :: MacOS X',
