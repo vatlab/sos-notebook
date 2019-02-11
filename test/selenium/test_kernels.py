3dc17b0cd95261e32b5d93a26d7c3fed0d19d9e4
Ma <jma7@mdanderson.org>
2019-02-11 14:00:23 -0600

    comment

--- test/selenium/test_kernels.py
+++ test/selenium/test_kernels.py
@@ -3,24 +3,22 @@ def test_shift_kernel(notebook):
 	kernels=notebook.get_kernel_list()
 	assert "SoS" in kernels
 	assert "R" in kernels
-	assert "Python 3" in kernels
 
-
+    #test shift to R kernel by click
 	notebook.shift_kernel(index=0,kernel_name="R",by_click=True)
 	command="%preview -n rn[1:3] \n rn <- rnorm(50)"
 	notebook.edit_cell(index=0,content=command,render=True)
 
 	assert "rn[1:3]" in notebook.get_cell_output(index=0)
 
-    
+    #test $get and shift to SoS kernel
 	command="%get rn --from R \n len(rn)"
 	notebook.append(command)
 	notebook.shift_kernel(index=1,kernel_name="SoS")
-	time.sleep(10)
 	notebook.execute_cell(1)
 	assert "50" in notebook.get_cell_output(index=1)
 
-   
+    
 
 
 
