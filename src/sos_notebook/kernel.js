6ce166f28194f05256b5e4fd83fb0f9898c028dc
Bo Peng <ben.bog@gmail.com>
2019-03-09 21:58:35 -0600

    Allow syntax hilighting of indented script format vatlab/sos#1233

--- src/sos_notebook/kernel.js
+++ src/sos_notebook/kernel.js
@@ -2572,7 +2572,7 @@ color: green;
         .concat(sosMagicWords);
 
       var sosDirectives = sosKeywords.map(x => x + ":");
-      var sosActions = sosActionWords.map(x => x + ":");
+      var sosActions = sosActionWords.map(x => new RegExp("^\\s*" + x + ":"));
       var sosMagics = sosMagicWords.map(x => '%' + x);
 
       // hint word for SoS mode
