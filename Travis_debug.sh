4bdecc7c364bfac677da685b2d5704d1793dc3ca
Bo Peng <ben.bog@gmail.com>
2019-04-02 14:56:03 -0500

    Move docker files etc to development folder

--- Travis_debug.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-curl -s -X POST  -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TOKEN" -d '{ "quiet": true }' https://api.travis-ci.org/job/$JOBID/debug  
