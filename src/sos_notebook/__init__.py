16e2c118d7eb05d0c12b8c427951d15a81b5585a
Bo <BoPeng@users.noreply.github.com>
2019-04-28 21:14:19 -0500

    Update __init__.py

---
+++ src/sos_notebook/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+from .version import __version__
