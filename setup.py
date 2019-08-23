0e8cbdb5e192ed7574f455b3f21635a696804529
Bo Peng <ben.bog@gmail.com>
2019-08-23 14:50:49 -0500

    Release sos notebook 0.19.17

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     package_dir={'': 'src'},
     python_requires='>=3.6',
     install_requires=[
-        'sos>=0.19.15',
+        'sos>=0.19.16',
         'nbformat',
         'nbconvert>=5.1.1',
         'ipython',
